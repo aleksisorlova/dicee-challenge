f4e7f6c1ff8c780f88cf8a9d7c3d6819b4004a14
aleksisorlova <aleor303@gmail.com>
2025-05-01 03:37:06 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+let image1 = "./images/dice" + randomNumber1 + ".png";
+
+document.querySelector(".img1").setAttribute("src", image1);
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+} else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
